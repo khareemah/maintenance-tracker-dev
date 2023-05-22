ff6d4677ae652e98b8c5e955eda9454bb566dce6
kareemah <ajimobikareemah@gmail.com>
2023-05-22 12:20:42 +0100

    add swaggwer docs

--- app.js
+++ app.js
@@ -11,8 +11,36 @@ const authRouter = require('./routes/authRoute');
 const userRequestRouter = require('./routes/userRequestRoute');
 const adminRequestRouter = require('./routes/adminRequestRoute');
 
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
+app.get('/', (req, res) => {
+  res.send(
+    res.send('<h1>E-commerce API</h1><a href="/api-docs">Documentation</a>')
+  );
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/requests', adminRequestRouter);
 app.use('/api/v1/users/requests', userRequestRouter);
